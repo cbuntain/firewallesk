ea00cf51a18af7cd550d5353d0adc9ad7969c0b6
Cody Buntain <cbuntain@primer.coretx.lan>
2019-07-18 23:48:41 -0400

    Index Creation for ES 6.7

---
+++ createIndex_6.7.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+import json
+import sys
+import requests
+
+index_info = {
+  "mappings": { 
+    "_doc": {
+      "properties": {
+        "source_ip": {
+          "type": "ip"
+        },
+        "destination_ip": {
+          "type": "ip"
+        },
+        "source_port": {
+          "type": "integer"
+        },
+        "destination_port": {
+          "type": "integer"
+        },
+        "date_time": {
+          "type": "date",
+          "format": "dd/MMM/yyyy HH:mm:ss"
+        }
+      }
+    }
+  }
+}
+
+es_url = sys.argv[1]
+index_name = sys.argv[2]
+
+res = requests.put(es_url + "/" + index_name, data=json.dumps(index_info), headers={"Content-Type": "application/json"})
+print("Result:", res.status_code, res.text)
